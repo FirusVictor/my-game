85ee6699dc825f557c23f21300dce10fd90243bb
firus.victor@gmail.com <mododel1>
2021-02-11 19:57:38 +0300

    added images

--- js/main.js
+++ js/main.js
@@ -453,3 +453,24 @@ let temp_name = '';
 
 const menu = new MenuUI();
 const game = new Game();
+
+function circleRect(cx, cy, radius, rx, ry, rw, rh) {
+
+    // temporary variables to set edges for testing
+    let testX = cx;
+    let testY = cy;
+
+    // which edge is closest?
+    if (cx < rx)         testX = rx;      // test left edge
+    else if (cx > rx+rw) testX = rx+rw;   // right edge
+    if (cy < ry)         testY = ry;      // top edge
+    else if (cy > ry+rh) testY = ry+rh;   // bottom edge
+
+    // get distance from closest edges
+    let distX = cx-testX;
+    let distY = cy-testY;
+    let distance = Math.sqrt( (distX*distX) + (distY*distY) );
+
+    // if the distance is less than the radius, collision!
+    return distance <= radius;
+}
